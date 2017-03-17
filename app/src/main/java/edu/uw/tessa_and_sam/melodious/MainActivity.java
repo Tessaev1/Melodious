bd1e2977f7dca738afa697241029000abab24a87
Sam Bender <rednebmas@gmail.com>
2017-03-17 11:52:23 -0700

    Setup & frequency detector

--- app/src/main/java/edu/uw/tessa_and_sam/melodious/MainActivity.java
+++ app/src/main/java/edu/uw/tessa_and_sam/melodious/MainActivity.java
@@ -1,13 +1,64 @@
 package edu.uw.tessa_and_sam.melodious;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PitchRecognizer.PitchRecognizerDelegate {
+    static final int MY_RECORD_AUDIO_PERMISSION_CONST = 927;
+    private PitchRecognizer pitchRecognizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pitchRecognizer = new PitchRecognizer();
+        pitchRecognizer.setDelegate(this);
+        this.checkPermissionsAndStartPitchRecognizer();
     }
+
+    public void pitchUpdated() {
+        Log.i("MainActivity", "freq: " + pitchRecognizer.getCurrentFreq());
+    }
+
+    private void checkPermissionsAndStartPitchRecognizer() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{ Manifest.permission.RECORD_AUDIO },
+                    MY_RECORD_AUDIO_PERMISSION_CONST);
+        }
+        else
+        {
+            pitchRecognizer.start();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults)
+    {
+        switch (requestCode) {
+            case MY_RECORD_AUDIO_PERMISSION_CONST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    this.pitchRecognizer.start();
+                }
+                else
+                {
+                    Log.i("MainActivity", "Audio Permission Denied");
+                }
+                return;
+            }
+        }
+    }
+
 }
